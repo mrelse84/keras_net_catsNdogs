68946db41fa5e196ce9a33dc75658dca615db5b6
sglee <mrelse84@naver.com>
2022-01-09 17:04:53 +0900

    used large file storage for the large model file

---
+++ keras_net_catsNdogs/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿
+namespace keras_net_catsNdogs
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label11 = new System.Windows.Forms.Label();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.btnLoadImage = new System.Windows.Forms.Button();
+            this.picImage = new System.Windows.Forms.PictureBox();
+            this.btnPredict = new System.Windows.Forms.Button();
+            this.btnLoadModel = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.panel4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(12, 9);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(148, 12);
+            this.label11.TabIndex = 16;
+            this.label11.Text = "Cats & Dogs Classification";
+            // 
+            // panel4
+            // 
+            this.panel4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel4.Controls.Add(this.label5);
+            this.panel4.Controls.Add(this.label4);
+            this.panel4.Controls.Add(this.label3);
+            this.panel4.Controls.Add(this.label2);
+            this.panel4.Controls.Add(this.btnLoadImage);
+            this.panel4.Controls.Add(this.picImage);
+            this.panel4.Controls.Add(this.btnPredict);
+            this.panel4.Controls.Add(this.btnLoadModel);
+            this.panel4.Location = new System.Drawing.Point(13, 28);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(380, 234);
+            this.panel4.TabIndex = 15;
+            // 
+            // btnLoadImage
+            // 
+            this.btnLoadImage.Location = new System.Drawing.Point(249, 108);
+            this.btnLoadImage.Name = "btnLoadImage";
+            this.btnLoadImage.Size = new System.Drawing.Size(115, 23);
+            this.btnLoadImage.TabIndex = 11;
+            this.btnLoadImage.Text = "Load Image";
+            this.btnLoadImage.UseVisualStyleBackColor = true;
+            this.btnLoadImage.Click += new System.EventHandler(this.btnLoadImage_Click);
+            // 
+            // picImage
+            // 
+            this.picImage.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.picImage.Location = new System.Drawing.Point(18, 19);
+            this.picImage.Name = "picImage";
+            this.picImage.Size = new System.Drawing.Size(194, 195);
+            this.picImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picImage.TabIndex = 10;
+            this.picImage.TabStop = false;
+            // 
+            // btnPredict
+            // 
+            this.btnPredict.Location = new System.Drawing.Point(249, 170);
+            this.btnPredict.Name = "btnPredict";
+            this.btnPredict.Size = new System.Drawing.Size(115, 23);
+            this.btnPredict.TabIndex = 9;
+            this.btnPredict.Text = "Predict";
+            this.btnPredict.UseVisualStyleBackColor = true;
+            this.btnPredict.Click += new System.EventHandler(this.btnPredict_Click);
+            // 
+            // btnLoadModel
+            // 
+            this.btnLoadModel.Location = new System.Drawing.Point(249, 46);
+            this.btnLoadModel.Name = "btnLoadModel";
+            this.btnLoadModel.Size = new System.Drawing.Size(115, 23);
+            this.btnLoadModel.TabIndex = 8;
+            this.btnLoadModel.Text = "Load Model";
+            this.btnLoadModel.UseVisualStyleBackColor = true;
+            this.btnLoadModel.Click += new System.EventHandler(this.btnLoadModel_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblResult.Location = new System.Drawing.Point(75, 276);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(318, 74);
+            this.lblResult.TabIndex = 18;
+            this.lblResult.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 307);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 12);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "Result :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(224, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(19, 12);
+            this.label2.TabIndex = 19;
+            this.label2.Text = "1 :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(224, 113);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(19, 12);
+            this.label3.TabIndex = 20;
+            this.label3.Text = "2 :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(224, 175);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(19, 12);
+            this.label4.TabIndex = 21;
+            this.label4.Text = "3 :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(217, 76);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(162, 12);
+            this.label5.TabIndex = 22;
+            this.label5.Text = "This may take some time...";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(410, 359);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.panel4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picImage)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Button btnLoadImage;
+        private System.Windows.Forms.PictureBox picImage;
+        private System.Windows.Forms.Button btnPredict;
+        private System.Windows.Forms.Button btnLoadModel;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ keras_net_catsNdogs/Form1.cs
@@ -0,0 +1,117 @@
+﻿using Keras.Models;
+using Numpy;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace keras_net_catsNdogs
+{
+    public partial class Form1 : Form
+    {
+        string app_startup_path = "";
+        string models_path = "";
+        string model_file = "";
+        Stopwatch sw = new Stopwatch();
+        NDarray x_normimg = null;
+        string[] class_names = null;
+        Keras.Models.BaseModel catsndogs_model = null;
+        byte[] byteImage = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            app_startup_path = Application.StartupPath;
+            int pos = app_startup_path.IndexOf("keras_net_catsNdogs\\bin");
+            models_path = app_startup_path.Substring(0, pos) + "models";
+
+            class_names = new string[] { "cat", "dog" };
+        }
+
+        private void btnLoadModel_Click(object sender, EventArgs e)
+        {
+            // model file : cats_and_dogs_xfer.h5
+            // Made by Transfer learning based on Xception model in python (jupyter notebook)
+            model_file = Path.Combine(models_path, "cats_and_dogs_xfer.h5");
+            sw.Reset();
+            sw.Start(); // start to measure time
+            catsndogs_model = Sequential.LoadModel(model_file);
+            if (sw.IsRunning) sw.Stop(); // end to measure time
+            string strResult = string.Format("LoadModel : {0} (Elapsed Time : {1:0.000} [msec])", model_file, sw.ElapsedTicks / Stopwatch.Frequency * 1E3);
+            lblResult.Text = strResult;
+        }
+
+        private void btnLoadImage_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Filter = "Images (*.JPG)|*.JPG|.BMP)|*.BMP|" + "All files (*.*)|*.*";
+            openFileDialog1.Title = "Select Cats & Dogs Image";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                Bitmap srcBitmap = new Bitmap(openFileDialog1.FileName);
+                int width = srcBitmap.Width;
+                int height = srcBitmap.Height;
+                Size resize = new Size(224, 224);
+                Bitmap myBitmap = new Bitmap(srcBitmap, resize);
+
+                picImage.Image = myBitmap;
+
+                // Image to Byte Array
+                Rectangle rect = new Rectangle(0, 0, myBitmap.Width, myBitmap.Height);
+                System.Drawing.Imaging.BitmapData bmpData =
+                    myBitmap.LockBits(rect, System.Drawing.Imaging.ImageLockMode.ReadWrite,
+                    System.Drawing.Imaging.PixelFormat.Format24bppRgb);
+
+                // Get the address of the first line.
+                IntPtr ptr = bmpData.Scan0;
+
+                // Declare an array to hold the bytes of the bitmap.
+                int bytes = Math.Abs(bmpData.Stride) * myBitmap.Height;
+                byteImage = new byte[bytes];
+                System.Runtime.InteropServices.Marshal.Copy(ptr, byteImage, 0, bytes);
+
+                // Unlock the bits.
+                myBitmap.UnlockBits(bmpData);
+            }
+        }
+
+        private void btnPredict_Click(object sender, EventArgs e)
+        {
+            if (catsndogs_model == null || picImage.Image == null)
+                return;
+
+            int n = byteImage.Length;
+            double[] doubleArr = new double[n];
+            int resize_difference = 9; // Approximate difference between resizing using opencv and resizing with bitmap .. not exact same
+            for (int i = 0; i < n; i++)
+            {
+                doubleArr[i] = (byteImage[i] + resize_difference) / (double)255;
+            }
+
+            sw.Reset();
+            sw.Start(); // start to measure time
+            x_normimg = np.array(doubleArr);
+            x_normimg = x_normimg.reshape(1, 224, 224, 3);
+            var pred = catsndogs_model.Predict(x_normimg);
+            if (sw.IsRunning) sw.Stop(); // end to measure time
+            var vmax = pred.argmax();
+            string str_max = vmax.str;
+            int imax = int.Parse(str_max);
+            float[] res = pred.GetData<float>();
+            string strResult = string.Format("Result : {0}, Probobility : {1:0.00}% (Elapsed Time : {2:0.000} [msec])", 
+                class_names[imax], res[imax] * 100, sw.ElapsedTicks / Stopwatch.Frequency * 1E3);
+            lblResult.Text = strResult;
+        }
+    }
+}
